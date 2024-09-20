33cfa183ea71237580ed3ef34c7696693e6c89da
kyungho0511 <kleearchitecture@gmail.com>
2024-09-20 12:01:57 -0400

    Import logo image as a module

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,16 +1,17 @@
 import { Link } from "react-router-dom";
 import styles from "./Logo.module.css";
+import logo from "../../assets/images/site_teller_black_logo.png";
 
 type LogoProps = {
   width?: string;
   color?: "white" | "black" | "blue";
 };
 
-export default function Logo({ width, color }: LogoProps) {
+export default function Logo({ width }: LogoProps) {
   return (
     <Link to="/" draggable={false}>
       <img
-        src={`/src/assets/images/site_teller_${color ?? "black"}_logo.png`}
+        src={logo}
         alt="logo_image"
         className={styles.logo}
         style={{ width: width }}
--- src/global.d.ts
+++ src/global.d.ts
@@ -11,3 +11,11 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv
 }
+
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.bmp';
+declare module '*.tiff';
+declare module '*.svg';
