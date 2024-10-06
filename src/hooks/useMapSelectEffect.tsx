9e1c0e25b9596f860162c25dbe542864ce455e24
kyungho0511 <kleearchitecture@gmail.com>
2024-10-05 23:08:40 -0400

    Update skipping non-selected features logic

--- src/hooks/useMapSelectEffect.tsx
+++ src/hooks/useMapSelectEffect.tsx
@@ -13,31 +13,36 @@ export default function useMapSelectEffect(layer: string, map?: mapboxgl.Map) {
   useEffectAfterMount(() => {
     if (!map) return;
     
-    const mouseEnterHandler = () => {
-      map.getCanvas().style.cursor = "pointer";
+    // Skip non-selected features(filled with white color).
+    const isSelectedFeature = (event: mapboxgl.MapMouseEvent): boolean => {
+      const feature = map.queryRenderedFeatures(event.point, {layers: [layer]})[0];
+      const fillColor = (feature?.layer?.paint as { "fill-color": FillColor })["fill-color"];
+      return !isWhiteFillColor(fillColor);
     }
+
     const mouseLeaveHandler = () => {
       map.getCanvas().style.cursor = "grab";
       mapbox.hideLineWidth(OUTLINE_LAYER, map);
     }
     const mouseMoveHandler = (event: mapboxgl.MapMouseEvent) => {
-      const feature = map.queryRenderedFeatures(event.point, {layers: [layer]})[0];
-      const fillColor = (feature.layer!.paint as { "fill-color": FillColor })["fill-color"];
-
-      // Skip features with white fill color (non-selected features).
-      if (!isWhiteFillColor(fillColor)) {
-        mapbox.setLineWidth(OUTLINE_LAYER, GEOID, feature.properties![GEOID], THICK_LINE_WEIGHT, map);
+      if (!isSelectedFeature(event)) {
+        map.getCanvas().style.cursor = "grab";
+        mapbox.hideLineWidth(OUTLINE_LAYER, map);
+        return;
       }
+      map.getCanvas().style.cursor = "pointer";
+      const feature = map.queryRenderedFeatures(event.point, {layers: [layer]})[0];
+      mapbox.setLineWidth(OUTLINE_LAYER, GEOID, feature.properties![GEOID], THICK_LINE_WEIGHT, map);
     }
 
     // Add event listeners.
-    map.on("mouseenter", layer, mouseEnterHandler);
+    // map.on("mouseenter", layer, mouseEnterHandler);
     map.on("mouseleave", layer, mouseLeaveHandler);
     map.on("mousemove", layer, mouseMoveHandler);
 
     // Cleanup event listeners on component unmount.
     return () => {
-      map.off("mouseenter", layer, mouseEnterHandler);
+      // map.off("mouseenter", layer, mouseEnterHandler);
       map.off("mouseleave", layer, mouseLeaveHandler);
       map.off("mousemove", layer, mouseMoveHandler);
     }
