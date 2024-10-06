5290d592e9a70958ec26e474e840dc86edcd28a9
kyungho0511 <kleearchitecture@gmail.com>
2024-10-05 21:48:44 -0400

    Skip non-selected feature for popups

--- src/components/organisms/PopupSection.tsx
+++ src/components/organisms/PopupSection.tsx
@@ -3,7 +3,8 @@ import styles from './PopupSection.module.css';
 import { MapContext } from '../../context/MapContext';
 import { SIDEBAR_WIDTH } from './Sidebar';
 import { FOOTBAR_HEIGHT } from './Footbar';
-import { POPUP } from '../../constants/mapConstants';
+import { FillColor, POPUP } from '../../constants/mapConstants';
+import { isWhiteFillColor } from '../../utils/utils';
 
 type Coordinate = {
   x: number;
@@ -22,46 +23,52 @@ export default function PopupSection({ children }: PopupSectionProps) {
   const [position, setPosition] = useState<Coordinate>({ x: 0, y: 0 });
   const [display, setDisplay] = useState<"block" | "none">("none");
 
+  const showPopup = () => {
+    setDisplay("block");
+  }
+
+  const hidePopup = () => {
+    setDisplay("none");
+  }
+
+  const updatePopupPosition = (event: mapboxgl.MapMouseEvent) => {
+    const mapWidth = window.innerWidth - SIDEBAR_WIDTH;
+    const mapOrigin = { x: SIDEBAR_WIDTH, y: 0 };
+    const translate = {x: 0, y: 0};
+
+    // Skip features with white fill color (non-selected features).
+    const feature = map!.queryRenderedFeatures(event.point, {layers: [parentLayer]})[0];
+    const fillColor = (feature.layer!.paint as { "fill-color": FillColor })["fill-color"];
+    if (isWhiteFillColor(fillColor)) {
+      hidePopup();
+      return;
+    } 
+    showPopup();
+
+    // Set X position of the popup.
+    if (event.point.x + POPUP.width + POPUP.offset > mapWidth) {
+      translate.x = event.point.x - POPUP.width - POPUP.offset;
+    } else {
+      translate.x = event.point.x + POPUP.offset;
+    }
+    // Set Y position of the popup.
+    if (event.point.y + POPUP.height - POPUP.offset > window.innerHeight - FOOTBAR_HEIGHT) {
+      translate.y = event.point.y - POPUP.height + POPUP.offset;
+    } else {
+      translate.y = event.point.y - POPUP.offset;
+    } 
+    setPosition({ x: translate.x + mapOrigin.x, y: translate.y + mapOrigin.y });
+  }
+
   // Set popup status based on the map mouse event.
   useEffect(() => {
     if (!map) return;
-
-    const updatePopupPosition = (event: mapboxgl.MapMouseEvent) => {
-      const mapWidth = window.innerWidth - SIDEBAR_WIDTH;
-      const mapOrigin = { x: SIDEBAR_WIDTH, y: 0 };
-      const translate = {x: 0, y: 0};
-
-      // Set X position of the popup.
-      if (event.point.x + POPUP.width + POPUP.offset > mapWidth) {
-        translate.x = event.point.x - POPUP.width - POPUP.offset;
-      } else {
-        translate.x = event.point.x + POPUP.offset;
-      }
-      // Set Y position of the popup.
-      if (event.point.y + POPUP.height - POPUP.offset > window.innerHeight - FOOTBAR_HEIGHT) {
-        translate.y = event.point.y - POPUP.height + POPUP.offset;
-      } else {
-        translate.y = event.point.y - POPUP.offset;
-      } 
-      setPosition({ x: translate.x + mapOrigin.x, y: translate.y + mapOrigin.y });
-    }
-
-    const showPopup = () => {
-      setDisplay("block");
-    }
-
-    const hidePopup = () => {
-      setDisplay("none");
-    }
-
     map.on("mousemove", parentLayer, updatePopupPosition);
-    map.on("mouseenter", parentLayer, showPopup);
     map.on("mouseleave", parentLayer, hidePopup);
 
     // Cleanup event listeners on component unmount.
     return () => {
       map.off("mousemove", parentLayer, updatePopupPosition);
-      map.off("mouseenter", parentLayer, showPopup);
       map.off("mouseleave", parentLayer, hidePopup);
     }
   }, [map, parentLayer]);
